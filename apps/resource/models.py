d8fe0aff9e54b215a8bc6985c0d1c2a16e52d068
Guqier <2535810484@qq.com>
2025-08-07 15:35:25 +0800

    feat: 引入资源模型类

--- apps/resource/models.py
+++ apps/resource/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from AAServer.common.models import BaseModel
+
+
 # Create your models here.
+class Resource(BaseModel):
+    id = models.BigIntegerField(primary_key=True, db_comment='资源ID')
+    name = models.CharField(max_length=255, blank=True, null=True, db_comment='资源名称')
+    type = models.IntegerField(blank=True, null=True, db_comment='资源类型，0为图片，1为pdf...')
+    group_name = models.CharField(max_length=255, blank=True, null=True, db_comment='文件组名')
+    remote_file_url = models.CharField(max_length=255, blank=True, null=True, db_comment='远程文件路径')
+    old_filename = models.CharField(max_length=255, blank=True, null=True, db_comment='源文件名')
+    sequence = models.IntegerField(blank=True, null=True, db_comment='同层级排序')
+
+
+    class Meta:
+        db_table = 'sys_resource'
+        db_table_comment = '文件资源表'
