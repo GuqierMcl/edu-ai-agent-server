17b3b80eb87fd597b088bbac2b34a346a20fc483
Guqier <2535810484@qq.com>
2025-08-11 13:24:02 +0800

    feat: 添加管理员权限获取逻辑

--- apps/auth/utils.py
+++ apps/auth/utils.py
@@ -13,17 +13,23 @@ from AAServer.utils.redis_utils import CacheKeys
 
 from apps.auth.models import Permission
 
+
 def get_user_perms_from_db(user) -> list:
     """
     从数据库获取用户权限
     :param user: 用户对象
     :return: set: 用户权限集合
     """
-    qs = Permission.objects.filter(
-        permissionrole__role__userrole__user=user,
-        permissionrole__role__userrole__user__is_del=0
-    ).values_list('name', flat=True).distinct()
+    if user.type == 0:  # 管理员
+        # 超级管理员拥有所有权限
+        qs = Permission.objects.all()
+    else:
+        qs = Permission.objects.filter(
+            permissionrole__role__userrole__user=user,
+            permissionrole__role__userrole__user__is_del=0
+        ).values_list('name', flat=True).distinct()
     return list(set(qs))
+
 
 def get_user_perms(user) -> list:
     """
