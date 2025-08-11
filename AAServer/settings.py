92a735de0f0740269e4ebe561bce589067fa8ad6
Guqier <2535810484@qq.com>
2025-08-11 14:34:02 +0800

    refactor: 重命名文件.

--- AAServer/settings.py
+++ AAServer/settings.py
@@ -110,7 +110,7 @@ REST_FRAMEWORK = {
         # "rest_framework.authentication.SessionAuthentication",
         # "rest_framework.authentication.TokenAuthentication",
         # "rest_framework.authentication.BasicAuthentication",
-        "AAServer.common.RedisTokenAuthentication.RedisTokenAuthentication",
+        "AAServer.common.authentication.RedisTokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
